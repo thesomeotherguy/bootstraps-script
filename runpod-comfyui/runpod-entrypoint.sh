4f6e52c1419e3017c379eb8a8512bc25e01bd03f
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-05 19:25:53 +0700

    Update and rename

--- runpod-comfyui/runpod-entrypoint.sh
+++ runpod-comfyui/runpod-entrypoint.sh
@@ -9,10 +9,10 @@ if [ ! -d /workspace/bootstraps-script ]; then
 fi
 
 echo "[INFO] Starting system services (SSH, Nginx, Jupyter)..."
-/bin/bash /workspace/bootstraps-script/runpod/start-runpod-services.sh
+/bin/bash /workspace/bootstraps-script/runpod-comfyui/start-runpod-services.sh
 
 echo "[INFO] Running ComfyUI bootstrap..."
-/bin/bash /workspace/bootstraps-script/runpod/comfyui-bootstrap.sh
+/bin/bash /workspace/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
 
 echo "[INFO] All setup done. Container is ready."
 sleep infinity
