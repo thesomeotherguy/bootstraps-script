1d6350e9a940b1c5c4ff26f1f6318025b965a6dc
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-05 20:58:50 +0700

    Update runpod-entrypoint.sh

--- runpod-comfyui/runpod-entrypoint.sh
+++ runpod-comfyui/runpod-entrypoint.sh
@@ -3,23 +3,11 @@
 set -e
 set -o pipefail
 
-echo "[INFO] Ensuring 'git' is installed..."
-apt-get update -y && apt-get install -y git
-
-if [ -d /workspace/bootstraps-script ]; then
-    echo "[INFO] Repo already exists, pulling latest changes..."
-    cd /workspace/bootstraps-script
-    git pull
-else
-    echo "[INFO] Cloning bootstrap repo (fresh)..."
-    git clone https://github.com/thesomeotherguy/bootstraps-script.git /workspace/bootstraps-script
-fi
-
 echo "[INFO] Starting system services (SSH, Nginx, Jupyter)..."
-/bin/bash /workspace/bootstraps-script/runpod-comfyui/start-runpod-services.sh
+/bin/bash /workspace/script/bootstraps-script/runpod-comfyui/start-runpod-services.sh
 
 echo "[INFO] Running ComfyUI bootstrap..."
-/bin/bash /workspace/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
+/bin/bash /workspace/script/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
 
 echo "[INFO] All setup done. Container is ready."
 sleep infinity
