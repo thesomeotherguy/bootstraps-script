f9e08f12f3cde460fba26fca816ff5eeb472d4e2
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-06 10:03:35 +0700

    Update runpod-entrypoint.sh

--- runpod-comfyui/runpod-entrypoint.sh
+++ runpod-comfyui/runpod-entrypoint.sh
@@ -10,3 +10,4 @@ echo "[INFO] Running ComfyUI bootstrap..."
 /bin/bash /workspace/script/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
 
 echo "[INFO] All setup done. Container is ready."
+# sleep infinity
