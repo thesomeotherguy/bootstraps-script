c7079dbd198312603f0433a8e9bb4ed36ca4a211
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-06 20:29:34 +0700

    Update runpod-entrypoint.sh

--- runpod-comfyui/runpod-entrypoint.sh
+++ runpod-comfyui/runpod-entrypoint.sh
@@ -3,11 +3,10 @@
 set -e
 set -o pipefail
 
-# echo "[INFO] Starting system services (SSH, Nginx, Jupyter)..."
-# /bin/bash /workspace/script/bootstraps-script/runpod-comfyui/start-runpod-services.sh
-
 echo "[INFO] Running ComfyUI bootstrap..."
 /bin/bash /workspace/script/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
 
+echo "[INFO] Starting system services (SSH, Nginx, Jupyter)..."
+/bin/bash /workspace/script/bootstraps-script/runpod-comfyui/start-runpod-services.sh
+
 echo "[INFO] All setup done. Container is ready."
-# sleep infinity
