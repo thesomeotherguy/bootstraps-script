91ce690669c4e8fef96d3c0cbb9d9861a7cbc25e
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-06 20:38:31 +0700

    Create runpod-entrypoint-uv.sh

---
+++ runpod-comfyui/runpod-entrypoint-uv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo "[INFO] Running ComfyUI bootstrap..."
+/bin/bash /workspace/script/bootstraps-script/runpod-comfyui/comfyui-bootstrap.sh
+
+echo "[INFO] Starting system services (SSH, Nginx, Jupyter)..."
+/bin/bash /workspace/script/bootstraps-script/runpod-comfyui/start-runpod-services-uv.sh
+
+echo "[INFO] All setup done. Container is ready."
