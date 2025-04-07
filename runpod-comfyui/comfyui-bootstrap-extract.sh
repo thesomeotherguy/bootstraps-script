ed504120c8dc350e432fa14852987ee8aefa33f3
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-07 15:09:17 +0700

    Update comfyui-bootstrap-extract.sh

--- runpod-comfyui/comfyui-bootstrap-extract.sh
+++ runpod-comfyui/comfyui-bootstrap-extract.sh
@@ -177,10 +177,12 @@ EOF
 # rm -rf /internalworkspace/ComfyUI/models
 # mv /internalworkspace/for-runpod-deploy/comfyui-models-folder /internalworkspace/ComfyUI/models
 
+echo "[INFO] Copying backup and upload scripts..."
 cd /internalworkspace
 cp /workspace/.script/bootstraps-script/runpod-comfyui/comfyui-venv-backup.sh /internalworkspace/comfyui-venv-backup.sh
 chmod +x comfyui-venv-backup.sh
 cp /workspace/.script/bootstraps-script/runpod-comfyui/comfyui-venv-backup-upload.py /internalworkspace/comfyui-venv-backup-upload.py
+cd /workspace
 
 echo "[INFO] Bootstrap completed. Deactivating virtual environment..."
 deactivate
