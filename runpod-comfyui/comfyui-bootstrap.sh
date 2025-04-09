98c813da202fc3d5a626d6d9ec6c3c02940e914c
thesomeotherguy <112531618+thesomeotherguy@users.noreply.github.com>
2025-04-09 22:05:47 +0700

    Update comfyui-bootstrap.sh

--- runpod-comfyui/comfyui-bootstrap.sh
+++ runpod-comfyui/comfyui-bootstrap.sh
@@ -146,6 +146,8 @@ cp /workspace/.script/bootstraps-script/runpod-comfyui/comfyui-venv-backup-uploa
 cd /workspace
 cp /workspace/.script/bootstraps-script/runpod-comfyui/workspace-backup.sh /workspace/workspace-backup.sh
 chmod +x workspace-backup.sh
+cp /workspace/.script/bootstraps-script/runpod-comfyui/show-comfyui-console.sh /workspace/show-comfyui-console.sh
+chmod +x show-comfyui-console.sh
 
 echo "[INFO] Configuring tmux for mouse support..."
 # Define the tmux config file path explicitly for clarity
